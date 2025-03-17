4b423b86a2740f23c127952b52c7e3dae99e24dc
Andrew <andrew@leftclick.io>
2025-03-18 00:29:56 +0200

    1. Cleaning-up folders, which might contain cached builds. 2. Installing `Vite` while building libraries.

--- ion-gateway.link.sh
+++ ion-gateway.link.sh
@@ -12,24 +12,29 @@ yarn link --register
 cd ..
 
 cd protocol || exit
+rm -rf lib node_modules
 yarn build
 npm link
 yarn link --register
 cd ..
 
 cd sdk || exit
+rm -rf dist lib node_modules
 yarn build
 npm link
 yarn link --register
 cd ..
 
 cd ui || exit
+rm -rf dist lib node_modules
+yarn add vite
 yarn build
 npm link
 yarn link --register
 cd ..
 
 cd ui-react || exit
+rm -rf lib node_modules
 yarn build
 npm link
 yarn link --register
